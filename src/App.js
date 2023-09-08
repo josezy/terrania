4021aa87723628ffbf13efe78f4f96439e682225
Jose B <jose.zdy@gmail.com>
2023-09-08 17:59:31 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+
+import samplePDF from "./terrania.pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.js'
 
 function App() {
+  const [numPages, setNumPages] = useState(null);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="row" style={{ height: "100vh", width: "100vw", display: "flex" }}>
+      <Document
+        file={samplePDF}
+        onLoadSuccess={onDocumentLoadSuccess}
+        onLoadError={console.error}
+      >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        ))}
+      </Document>
     </div>
   );
 }
